417b15ae3681865478e4d2dedbc51967d0e41379
Cameron McDrury <cjm372@uclive.ac.nz>
2021-03-21 16:42:22 +1300

    Displays need to switch between

--- Main.c
+++ Main.c
@@ -3,6 +3,7 @@
 
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
@@ -25,6 +26,7 @@
 /**********************************************************
  * Constants
  **********************************************************/
+
 // Systick configuration
 #define SYSTICK_RATE_HZ    100
 
@@ -55,8 +57,10 @@
 #define BUF_SIZE 10
 #define SAMPLE_RATE_HZ 10
 
-
 #define OLED_REFRESH_DIVIDER    60
+
+
+
 /*******************************************
  *      Local prototypes
  *******************************************/
@@ -79,100 +83,30 @@ static uint32_t g_ulSampCnt;    // Counter for the interrupts
 /***********************************************************
  * ISR for the SysTick interrupt (used for button debouncing).
  ***********************************************************/
+//*****************************************************************************
+//
+// The interrupt handler for the for SysTick interrupt.
+//
+//*****************************************************************************
 void
-SysTickIntHandler (void)
+SysTickIntHandler(void)
 {
-    // Borrowed from pwmGen (Lab 2)
     //
-    // Poll the buttons
-    updateButtons();
+    // Initiate a conversion
     //
-    // It is not necessary to clear the SysTick interrupt.
+    ADCProcessorTrigger(ADC0_BASE, 3);
+    g_ulSampCnt++;
 }
 
-/***********************************************************
- * Initialisation functions: clock, SysTick, PWM
- ***********************************************************
- * Clock
- ***********************************************************/
-void
-initClocks (void)
-{
-    // Borrowed from pwmGen (Lab 2)
-    // Set the clock rate to 20 MHz
-    SysCtlClockSet (SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
-                   SYSCTL_XTAL_16MHZ);
-
-    // Set the PWM clock rate (using the prescaler)
-    SysCtlPWMClockSet(PWM_DIVIDER_CODE);
-}
-
-
-
-/*************************************************************
- * SysTick interrupt
- ************************************************************/
-void
-initSysTick (void)
-{
-    // Borrowed from pwmGen (Lab 2)
-    //
-    // Set up the period for the SysTick timer.  The SysTick
-    // timer period is set as a function of the system clock.
-    SysTickPeriodSet (SysCtlClockGet() / SYSTICK_RATE_HZ);
-    //
-    // Register the interrupt handler
-    SysTickIntRegister (SysTickIntHandler);
-    //
-    // Enable interrupt and device
-    SysTickIntEnable ();
-    SysTickEnable ();
-}
-
-/*********************************************************
- * initialisePWM
- * M0PWM7 (J4-05, PC5) is used for the main rotor motor
- *********************************************************/
-void
-initialisePWM (void)
-{
-    // Borrowed from pwmGen (Lab 2)
-    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_PWM);
-    SysCtlPeripheralEnable(PWM_MAIN_PERIPH_GPIO);
-
-    GPIOPinConfigure(PWM_MAIN_GPIO_CONFIG);
-    GPIOPinTypePWM(PWM_MAIN_GPIO_BASE, PWM_MAIN_GPIO_PIN);
-
-    PWMGenConfigure(PWM_MAIN_BASE, PWM_MAIN_GEN,
-                    PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
-    // Set the initial PWM parameters
-    setPWM (PWM_START_RATE_HZ, PWM_START_DUTY);
-
-    PWMGenEnable(PWM_MAIN_BASE, PWM_MAIN_GEN);
-
-    // Disable the output.  Repeat this call with 'true' to turn O/P on.
-    PWMOutputState(PWM_MAIN_BASE, PWM_MAIN_OUTBIT, false);
-}
-
-/********************************************************
- * Function to set the freq, duty cycle of M0PWM7
- ********************************************************/
-void
-setPWM (uint32_t ui32Freq, uint32_t ui32Duty)
-{
-    // Borrowed from pwmGen (Lab 2)
-    // Calculate the PWM period corresponding to the freq.
-    uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / ui32Freq;
-
-    PWMGenPeriodSet(PWM_MAIN_BASE, PWM_MAIN_GEN, ui32Period);
-    PWMPulseWidthSet(PWM_MAIN_BASE, PWM_MAIN_OUTNUM,
-        ui32Period * ui32Duty / 100);
-}
-
+//*****************************************************************************
+//
+// The handler for the ADC conversion complete interrupt.
+// Writes to the circular buffer.
+//
+//*****************************************************************************
 void
 ADCIntHandler(void)
 {
-    // Borrowed from ADCdemo1.c
     uint32_t ulValue;
 
     //
@@ -187,10 +121,31 @@ ADCIntHandler(void)
     ADCIntClear(ADC0_BASE, 3);
 }
 
+//*****************************************************************************
+// Initialisation functions for the clock (incl. SysTick), ADC, display
+//*****************************************************************************
+void
+initClock (void)
+{
+    // Set the clock rate to 20 MHz
+    SysCtlClockSet (SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+    //
+    // Set up the period for the SysTick timer.  The SysTick timer period is
+    // set as a function of the system clock.
+    SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
+    //
+    // Register the interrupt handler
+    SysTickIntRegister(SysTickIntHandler);
+    //
+    // Enable interrupt and device
+    SysTickIntEnable();
+    SysTickEnable();
+}
+
 void
 initADC (void)
 {
-    // Borrowed from ADCdemo1
     //
     // The ADC0 peripheral must be enabled for configuration and use.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
@@ -209,7 +164,7 @@ initADC (void)
     // sequence 0 has 8 programmable steps.  Since we are only doing a single
     // conversion using sequence 3 we will only configure step 0.  For more
     // on the ADC sequences and steps, refer to the LM3S1968 datasheet.
-    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE | // CHANGE HERE FOR LAB
                              ADC_CTL_END);
 
     //
@@ -225,52 +180,144 @@ initADC (void)
     ADCIntEnable(ADC0_BASE, 3);
 }
 
-
 void
 initDisplay (void)
 {
-    // Borrowed from ADCdemo1
     // intialise the Orbit OLED display
     OLEDInitialise ();
 }
 
-// our functions
 
-// P control
-// D control
-// I Control (Do we need this?)
+void
+displayUpdate (char *str1, char *str2, uint32_t num, uint8_t charLine)
+{
+    char text_buffer[17];           //Display fits 16 characters wide.
+
+    // "Undraw" the previous contents of the line to be updated.
+    OLEDStringDraw ("                ", 0, charLine);
+    // Form a new string for the line.  The maximum width specified for the
+    //  number field ensures it is displayed right justified.
+    usnprintf(text_buffer, sizeof(text_buffer), "%s %s %3d", str1, str2, num);
+    // Update line on display.
+    OLEDStringDraw (text_buffer, 0, charLine);
+}
+
+//*****************************************************************************
+//
+// Function to display the mean ADC value (10-bit value, note) and sample count.
+//
+//*****************************************************************************
+void
+displayAltPercent(int32_t sum, uint32_t count, uint16_t voltageLanded, uint16_t voltageMaxHeight)
+{
+    char string[17];  // 16 characters across the display
 
 
-void main() {
+
+    OLEDStringDraw ("Heli Control", 0, 0);
+    // This works
+    uint32_t percent =  100 - 100*(((2 * sum + BUF_SIZE) / 2 / BUF_SIZE) - voltageMaxHeight)/(voltageLanded-voltageMaxHeight);
+
+    // Form a new string for the line.  The maximum width specified for the
+    //  number field ensures it is displayed right justified.
+    usnprintf (string, sizeof(string), "Height pc = %4d", percent);
+    // Update line on display.
+    OLEDStringDraw (string, 0, 1);
+
+    usnprintf (string, sizeof(string), "Sample # %5d", count);
+    OLEDStringDraw (string, 0, 3);
+}
+
+void
+displayMeanADC(int32_t sum, uint32_t count, uint16_t voltageLanded, uint16_t voltageMaxHeight)
+{
+    char string[17];  // 16 characters across the display
+
+    OLEDStringDraw ("Heli Control", 0, 0);
+    // This works
+    uint32_t mean =  (2 * sum + BUF_SIZE) / 2 / BUF_SIZE;
+
+    // Form a new string for the line.  The maximum width specified for the
+    //  number field ensures it is displayed right justified.
+    usnprintf (string, sizeof(string), "Mean ADC = %4d", mean);
+    // Update line on display.
+    OLEDStringDraw (string, 0, 1);
+
+    usnprintf (string, sizeof(string), "Sample # %5d", count);
+    OLEDStringDraw (string, 0, 3);
+}
+
+void
+displayOff(void)
+{
+    // Blank the dispaly
+    OLEDStringDraw("", 0, 0);
+    OLEDStringDraw("", 0, 1);
+    OLEDStringDraw("", 0, 2);
+    OLEDStringDraw("", 0, 3);
+}
+
+int
+main(void)
+{
+
+
 
     uint16_t i;
     int32_t sum;
-
+    SysCtlPeripheralReset (UP_BUT_PERIPH);        // UP button GPIO
+    SysCtlPeripheralReset (DOWN_BUT_PERIPH);      // DOWN button GPIO
+    initButtons ();
     initClock ();
     initADC ();
     initDisplay ();
+
     initCircBuf (&g_inBuffer, BUF_SIZE);
 
-    //
+
+
     // Enable interrupts to the processor.
     IntMasterEnable();
+    SysCtlDelay (SysCtlClockGet() / 6);
+    // Read the landed ADC.
+    uint16_t voltageLanded = readCircBuf(&g_inBuffer);
+    uint16_t voltageMaxHeight = 0 ;
+
+    uint8_t flag = 1; // 0 for percent, 1 for adc or 2 for off
 
     while (1)
     {
         //
+
+
+        if (checkButton(UP) == PUSHED) {
+
+            OLEDStringDraw("PLEEBUS", 0, 0);
+            //SysCtlDelay (SysCtlClockGet() / 6);
+            flag = flag + 1;
+            if (flag == 3) {
+                flag = 0;
+            }
+        }
+
         // Background task: calculate the (approximate) mean of the values in the
         // circular buffer and display it, together with the sample number.
         sum = 0;
         for (i = 0; i < BUF_SIZE; i++)
             sum = sum + readCircBuf (&g_inBuffer);
-        // Calculate and display the rounded mean of the buffer contents
-        displayMeanVal ((2 * sum + BUF_SIZE) / 2 / BUF_SIZE, g_ulSampCnt);
+
+
+        //displayMeanADC (sum, g_ulSampCnt, voltageLanded, voltageMaxHeight);
+        if (flag == 0) {
+            displayAltPercent(sum, g_ulSampCnt, voltageLanded, voltageMaxHeight);
+        }   else if (flag == 1) {
+            displayMeanADC(sum, g_ulSampCnt, voltageLanded, voltageMaxHeight);
+        }   else {
+            displayOff();
+        }
+
 
         SysCtlDelay (SysCtlClockGet() / 6);  // Update display at ~ 2 Hz
     }
-
-
-
 }
-
 
