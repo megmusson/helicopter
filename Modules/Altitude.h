2d3094dcd7c36db4e2cca77fa179e811b3d792ff
Meg Musson <mkm90@uclive.ac.nz>
2021-05-21 16:39:35 +1200

    Update Altitude.h

--- Modules/Altitude.h
+++ Modules/Altitude.h
@@ -13,23 +13,38 @@
 #include <stdbool.h>
 #include "../circBufT.h"
 
+// name of buffer
 circBuf_t g_inBuffer;
 
+// *******************************************************
+// The handler for the ADC conversion complete interrupt.
+// Writes to the circular buffer.
 void
 ADCIntHandler(void);
 
+// *******************************************************
+// Initialise the variables associated with the ADC
+// defined by the constants in Altitude.c.
 void
 initADC (void);
 
+// *******************************************************
+// Calculates the altitude of the helicopter as a percentage relative to its maximum and minimum height
 int32_t
 calcAltPercent(void);
 
+// *******************************************************
+//Calculates and returns the rounded average altitude of the helicopter
 int32_t
 calcAltAverage(void);
 
+// *******************************************************
+//Sets the maximum and minimum voltage value for altitude 
 void
 setMinMaxAlt(void);
 
+// *******************************************************
+//Sums all the values in buffer to determine average altitude over BUF_SIZE samples
 void
 readAltitude(void);
 
